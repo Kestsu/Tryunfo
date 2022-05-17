f30451d4cd28a6728274cde8f7fd271148740a13
Kevin Li <Kevinstart14@gmail.com>
2022-05-16 22:03:43 -0300

    refiz 6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
       cardRare: 'Normal',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
       baralho: [],
     };
     this.handleChange = this.handleChange.bind(this);
@@ -27,7 +27,7 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.fusion());
   };
 
   handleSave = (event) => {
@@ -77,6 +77,47 @@ class App extends React.Component {
     }
   };
 
+  handleEmpty = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    if (
+      cardName.length === 0
+            || cardDescription.length === 0
+            || cardImage.length === 0
+            || cardRare.length === 0
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  handleLenght = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const attr1 = Number(cardAttr1);
+    const attr2 = Number(cardAttr2);
+    const attr3 = Number(cardAttr3);
+    const nove = 90;
+    const zero = 0;
+    const max = 210;
+    if (attr1 > nove
+        || attr2 > nove
+        || attr3 > nove
+        || attr1 < zero
+        || attr2 < zero
+        || attr3 < zero
+        || attr1 + attr2 + attr3 > max) {
+      return true;
+    }
+    return false;
+  }
+
+  fusion = () => {
+    const resultadoValue = this.handleLenght();
+    const resultadoEmpty = this.handleEmpty();
+    if (resultadoValue === false && resultadoEmpty === false) {
+      return this.setState({ isSaveButtonDisabled: false });
+    } return this.setState({ isSaveButtonDisabled: true });
+  }
+
   render() {
     const {
       cardName,
@@ -88,31 +129,32 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
 
-    let teste = 'ativar';
+    // let teste = 'ativar';
 
-    if (
-      cardName.length === 0
-        || cardDescription.length === 0
-        || cardImage.length === 0
-        || cardRare.length === 0
-    ) {
-      teste = 'desativar';
-    }
+    // if (
+    //   cardName.length === 0
+    //         || cardDescription.length === 0
+    //         || cardImage.length === 0
+    //         || cardRare.length === 0
+    // ) {
+    //   teste = 'desativar';
+    // }
 
-    if (cardAttr1 + cardAttr2 + cardAttr3 > '210') {
-      teste = 'desativar';
-    }
-    if (cardAttr1 > '90'
-    || cardAttr2 > '90'
-    || cardAttr3 > '90'
-    || cardAttr1 < '0'
-    || cardAttr2 < '0'
-    || cardAttr3 < '0'
-    ) {
-      teste = 'desativar';
-    }
+    // if (cardAttr1 + cardAttr2 + cardAttr3 > '210') {
+    //   teste = 'desativar';
+    // }
+    // if (cardAttr1 > '90'
+    //     || cardAttr2 > '90'
+    //     || cardAttr3 > '90'
+    //     || cardAttr1 < '0'
+    //     || cardAttr2 < '0'
+    //     || cardAttr3 < '0'
+    // ) {
+    //   teste = 'desativar';
+    // }
 
     return (
       <div>
@@ -126,7 +168,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
-          isSaveButtonDisabled={ teste === 'desativar' }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.handleChange }
           onSaveButtonClick={ this.handleSave }
         />
@@ -146,3 +188,24 @@ class App extends React.Component {
 }
 
 export default App;
+
+// btn = () => {
+//   if
+//   ((cardAttr1 > '90')
+//   || (cardAttr2 > '90')
+//   || (cardAttr3 > '90')
+//   || (cardAttr1 < 0)
+//   || (cardAttr2 < 0)
+//   || (cardAttr3 < 0)
+//   || (cardAttr1 + cardAttr2 + cardAttr3 > '210')
+//   || (cardName.length === 0)
+//   || (cardDescription.length === 0)
+//   || (cardImage.length === 0)
+//   || (cardRare.length === 0)
+//   ) {
+//     return this.setState({
+//       isSaveButtonDisabled: false,
+//     });
+//   }
+//   this.setState({ isSaveButtonDisabled: true });
+// }
